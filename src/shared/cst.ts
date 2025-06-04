0318eb8244b4a91ccb8d491cff761c91c28493ba
AndyC5H10O5 <1915965336@qq.com>
2025-06-04 19:29:48 +0800

    修复bug，支持快捷键和按钮打开补全窗口，并在修改代码时更新内容。

--- src/shared/cst.ts
+++ src/shared/cst.ts
@@ -1,4 +1,5 @@
 import { CSTItem } from "../types/context";
+import { History } from "../core/context/history";
 
 export const fileDeclarations: { [key: string]: string[] } = {};
 export const CSTItems: CSTItem[] = [];
