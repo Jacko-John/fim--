13d75e653253b9306b3cd503db249185d01fcef1
Jacko-John <1730072668@qq.com>
2025-06-08 14:53:24 +0800

    refactor: 重构历史记录管理逻辑，优化文件删除时的历史记录更新

--- src/core/context/history.ts
+++ src/core/context/history.ts
@@ -1,13 +1,17 @@
 export class History {
   current: string[] = [];
   addHistory(filePath: string): void {
-    const idx = this.current.indexOf(filePath);
-    if (idx !== -1) {
-      this.current.splice(idx, 1);
-    }
+    this.deleteHistory(filePath);
     const length = this.current.unshift(filePath);
     if (length > 10) {
       this.current.pop();
+    }
+  }
+
+  deleteHistory(filePath: string): void {
+    const idx = this.current.indexOf(filePath);
+    if (idx !== -1) {
+      this.current.splice(idx, 1);
     }
   }
 
